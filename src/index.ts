26d2bf604bb2b102d4b3214f282929c3f126cb77
Eric Cui <ecui@gmail.com>
2022-07-29 02:53:28 -0700

    added: cors

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ import { ApolloServer } from 'apollo-server'
 import { schema } from './schema'
 import { context } from './context'
 
+const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true
+};
+
 export const server = new ApolloServer({
     schema,
     context,
+    cors: corsOptions
 })
 
 const port = 5000;
